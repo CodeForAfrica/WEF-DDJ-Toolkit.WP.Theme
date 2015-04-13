1971d903d0f74f5177cba505556814c635fbd8b5
David Lemayian <david@codeforafrica.org>
2015-04-13 05:28:32 +0300

    Hardcoded Google Analytics

--- footer.php
+++ footer.php
@@ -48,5 +48,17 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
   </div> <!-- #page-container -->
 
   <?php wp_footer(); ?>
+
+  <!-- Google Analytics -->
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44795600-16', 'auto');
+  ga('send', 'pageview');
+  </script>
+
 </body>
 </html>
